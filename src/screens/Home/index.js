b0cd74d057565598966ff0bfd4ba0c63d0520a1e
nguyenductoan1995 <nguyenductoann@gmail.com>
2019-03-22 11:15:47 +0700

    Camera

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,28 +1,55 @@
 import React, { Component } from 'react'
 import { View, Text, TouchableOpacity } from 'react-native'
+import { RNCamera } from 'react-native-camera'
 
 class Home extends Component {
-  static navigationOptions = {
-    header: null,
+static navigationOptions = {
+  header: null,
+}
+
+constructor(props) {
+  super(props)
+  this.state = {
+
   }
+}
 
-  constructor(props) {
-    super(props)
-    this.state = {
+takePicture =() => {
+  this.camera.takePictureAsync()
+    .then((data) => { console.log(data) })
+    .catch((err) => {})
+}
 
-    }
-  }
-
-  render() {
-    const { navigation } = this.props
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <TouchableOpacity onPress={() => { navigation.navigate('Chat') }}>
-          <Text>Toan</Text>
+render() {
+  const { navigation } = this.props
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <RNCamera
+        ref={(ref) => {
+          this.camera = ref
+        }}
+        style={{
+          width: 200,
+          height: 200,
+        }}
+        type={RNCamera.Constants.Type.back}
+        flashMode={RNCamera.Constants.FlashMode.auto}
+        permissionDialogTitle="Permission to use camera"
+        permissionDialogMessage="We need your permission to use your camera phone"
+        onGoogleVisionBarcodesDetected={({ barcodes }) => {
+          console.log(barcodes)
+        }}
+        onFacesDetected={response => console.log(response)}
+        onTextRecognized={response => console.log(response)}
+      />
+      <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+        <TouchableOpacity onPress={this.takePicture} style={{}}>
+          <Text style={{ fontSize: 14 }}> SNAP </Text>
         </TouchableOpacity>
       </View>
-    )
-  }
+    </View>
+  )
+}
 }
 
 export default Home
